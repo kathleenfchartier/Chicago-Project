3727f2340501c0e1ed50218ff4ccebee1ae66a49
kathleenfchartier <119955400+kathleenfchartier@users.noreply.github.com>
2023-10-06 12:19:13 -0400

    Update crimes by year graph.sql
    Added query description

--- crimes by year graph.sql
+++ crimes by year graph.sql
@@ -1,4 +1,4 @@
-
+/*  Count the number of crimes by Year, create bar graph as visual */
 
 select `Year`, count(`Year`) as numb_crimes,  REPEAT('*', count(`Year`)/10000) as 'Graph'
 from `chicago_crime_data`
