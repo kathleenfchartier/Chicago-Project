069dbf8a4a81aa91b5543eeac71e8d9b97da444c
kathleenfchartier <119955400+kathleenfchartier@users.noreply.github.com>
2023-10-06 12:25:19 -0400

    Update crimes area empty and year.sql
    Added query description

--- crimes area empty and year.sql
+++ crimes area empty and year.sql
@@ -1,10 +1,6 @@
+/*  Query crime count by Year for crimes without Comminity area  */
 
 select `Community Area`, `Year`, count(`ID`) as crime_count
 from chicago_crime_data
 where `Community Area` is null or `Community Area`=0
 group by `Community Area`, `Year`;
-
-select `Community Area`, `Year`, `Updated On`, count(`ID`)
-from chicago_crime_data
-where `Community Area` is null and `Year`=2001
-group by `Community Area`, `Year`, `Updated On`;
